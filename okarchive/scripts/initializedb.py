80d1f0e7155c2a8b42ad816dc492b6c32777a1a4
Joel Burton <joel@joelburton.com>
2013-10-16 07:55:25 -0700

    Add pragmas for initialize remaining untested.

--- okarchive/scripts/initializedb.py
+++ okarchive/scripts/initializedb.py
@@ -17,14 +17,14 @@ from ..models import (
     )
 
 
-def usage(argv):
+def usage(argv): #pragma NOCOVER
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
 
-def main(argv=sys.argv):
+def main(argv=sys.argv): #pragma NOCOVER
     if len(argv) != 2:
         usage(argv)
     config_uri = argv[1]
