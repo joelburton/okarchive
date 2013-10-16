b04cdf285718353658885210990bd9b897793aa1
Joel Burton <joel@joelburton.com>
2013-10-15 21:41:41 -0700

    Add post-add form and view.

--- okarchive/views.py
+++ okarchive/views.py
@@ -1,6 +1,9 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import (
+    HTTPFound,
+    HTTPNotFound,
+)
 
 from sqlalchemy.exc import DBAPIError
 
@@ -11,38 +14,66 @@ from .models import (
     )
 
 
-@view_config(route_name='journal', renderer='templates/journal.pt')
+@view_config(route_name='journal',
+             renderer='templates/journal.pt')
 def journal_view(request):
     journal_name = request.matchdict['journal_name']
-    try:
-        journal = DBSession.query(Journal).filter(Journal.name == journal_name).first()
-    except DBAPIError:
-        return Response("No such journal", content_type='text/plain', status_int=500)
+    journal = DBSession.query(Journal)\
+        .filter(Journal.name == journal_name)\
+        .first()
+    if not journal:
+        raise HTTPNotFound('No such journal.')
 
     return {'journal_name': journal_name, 'posts': journal.posts}
 
 
 class PostView(object):
+    """View and edit view for posts."""
+
     def __init__(self, request):
         self.request = request
 
-        journal_name = request.matchdict['journal_name']
-        post_id = request.matchdict['post_id']
-        try:
-            self.post = DBSession.query(Post).filter(Post.journal_name == journal_name).\
-                filter(Post.id == post_id).first()
-        except DBAPIError:
-            return Response('No such post', content_type='text/plain', status_int=500)
+    def _get_post(self):
+        journal_name = self.request.matchdict['journal_name']
+        post_id = self.request.matchdict['post_id']
+        post = DBSession.query(Post)\
+            .filter(Post.journal_name == journal_name)\
+            .filter(Post.id == post_id)\
+            .first()
+        if not post:
+            raise HTTPNotFound('No such post.')
+        return post
 
-    @view_config(route_name='post', renderer='templates/post.pt')
+    @view_config(route_name='post',
+                 renderer='templates/post.pt')
     def view(self):
-        return {'post': self.post}
+        return {'post': self._get_post()}
 
-    @view_config(route_name='post_edit', renderer='templates/post_edit.pt')
+    @view_config(route_name='post_edit',
+                 renderer='templates/post_edit.pt')
     def edit(self):
+        post = self._get_post()
         if 'form.Submitted' in self.request.params:
-            self.post.title = self.request.POST['title']
-            self.post.text = self.request.POST['text']
-            return HTTPFound(location=self.request.route_url(
-                'post', journal_name=self.post.journal_name, post_id=self.post.id))
-        return {'post': self.post, 'action': self.request.url}
+            post.title = self.request.POST['title']
+            post.text = self.request.POST['text']
+            return HTTPFound(location=self.request.route_url('post',
+                                                             journal_name=post.journal_name,
+                                                             post_id=post.id))
+        return {'post': post, 'action': self.request.url}
+
+    @view_config(route_name='post_add',
+                 renderer='templates/post_edit.pt')
+    def add(self):
+        if 'form.Submitted' in self.request.params:
+            post = Post(title=self.request.POST['title'],
+                        text=self.request.POST['text'],
+                        journal_name=self.request.matchdict['journal_name'])
+            DBSession.add(post)
+            DBSession.flush()
+            return HTTPFound(location=self.request.route_url('post',
+                                                             journal_name=post.journal_name,
+                                                             post_id=post.id))
+
+        post = Post(title='Title',
+                    journal_name=self.request.matchdict['journal_name'])
+        return {'post': post, 'action': self.request.url}
